a649f2dc189acb6176aabad63067886318d38f93
OmarAsharf62 <omarbondok62@gmail.com>
2024-03-01 23:30:41 +0200

    finished the search and the baner part one

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:fake_store_app/consts/global_colors.dart';
 import 'package:fake_store_app/widgets/appbar_icon.dart';
+import 'package:fake_store_app/widgets/sale_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 
@@ -10,6 +12,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _textEditingController;
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -31,7 +46,43 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-        body: const Center(child: Text("Welcome to this course")),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 18,
+              ),
+              TextField(
+                controller: _textEditingController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  filled: true,
+                  fillColor: Theme.of(context).cardColor,
+                  suffixIcon: Icon(
+                    IconlyLight.search,
+                    color: lightIconsColor,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).cardColor,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+              ),
+              const SaleWidget(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/sale_widget.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class SaleWidget extends StatelessWidget {
+  const SaleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        gradient: const LinearGradient(
+          colors: [
+            Color(0xFF7A60A5),
+            Color(0xFF82C3DF),
+          ],
+          begin: FractionalOffset(0.0, 0.0),
+          end: FractionalOffset(1.0, 0.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp,
+        ),
+      ),
+      child: Row(children: [
+        Flexible(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  color: const Color(0xFF7A60A5),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Get a Specail Discount",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Flexible(
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: FittedBox(
+                            fit: BoxFit.fill,
+                            child: Text(
+                              "50% OFF",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )),
+        Flexible(
+          flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Image.network(
+              'https://i.ibb.co/vwB46Yq/shoes.png',
+              width: double.infinity,
+              //height: double.infinity,
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
