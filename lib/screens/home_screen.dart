6d625ce4748f01ae2e803ba98c8faf7fd3c773bc
OmarAsharf62 <omarbondok62@gmail.com>
2024-03-07 08:06:13 +0200

    impelement the products gridView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,8 @@
-import 'package:fake_store_app/consts/global_colors.dart';
 import 'package:fake_store_app/widgets/appbar_icon.dart';
 import 'package:fake_store_app/widgets/latest_products_widget.dart';
+import 'package:fake_store_app/widgets/product_item.dart';
 import 'package:fake_store_app/widgets/search_field_widget.dart';
 import 'package:fake_store_app/widgets/swiper_widget.dart';
-import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 
@@ -54,102 +52,30 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 18,
-              ),
-              SearchFieldWidget(textEditingController: _textEditingController),
-              SwiperWidget(size: size),
-              LatestProducts(function: () {}),
-              SizedBox(
-                width: 180,
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Theme.of(context).cardColor,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: InkWell(
-                        borderRadius: BorderRadius.circular(10),
-                        onTap: () {},
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 5, right: 5, top: 8),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Flexible(
-                                    child: RichText(
-                                      text: TextSpan(
-                                        text: "\$",
-                                        style: const TextStyle(
-                                          color:
-                                              Color.fromRGBO(33, 150, 243, 1),
-                                        ),
-                                        children: <TextSpan>[
-                                          TextSpan(
-                                              text: '169.00',
-                                              style: TextStyle(
-                                                color: lightTextColor,
-                                                fontWeight: FontWeight.w600,
-                                              )),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  const Icon(IconlyBold.heart),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
-                              child: FancyShimmerImage(
-                                imageUrl: 'https://i.ibb.co/vwB46Yq/shoes.png',
-                                height: size.height * 0.2,
-                                width: double.infinity,
-                                boxFit: BoxFit.fill,
-                                errorWidget: const Icon(
-                                  IconlyBold.danger,
-                                  color: Colors.red,
-                                  size: 22,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Text(
-                                'Titiel',
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 2,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: size.height * 0.001,
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 18,
                 ),
-              ),
-            ],
+                SearchFieldWidget(
+                    textEditingController: _textEditingController),
+                SwiperWidget(size: size),
+                LatestProducts(function: () {}),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 5,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 4,
+                    mainAxisSpacing: 4,
+                    childAspectRatio: 0.6,
+                  ),
+                  itemBuilder: (context, index) => const ProductItemWidget(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,97 @@
+import 'package:fake_store_app/consts/global_colors.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+class ProductItemWidget extends StatelessWidget {
+  const ProductItemWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Material(
+        
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).cardColor,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(10),
+            onTap: () {},
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 5, right: 5, top: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: RichText(
+                          text: TextSpan(
+                            text: "\$",
+                            style: const TextStyle(
+                              color: Color.fromRGBO(33, 150, 243, 1),
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: '169.00',
+                                  style: TextStyle(
+                                    color: lightTextColor,
+                                    fontWeight: FontWeight.w600,
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Icon(IconlyBold.heart),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: FancyShimmerImage(
+                    imageUrl: 'https://i.ibb.co/vwB46Yq/shoes.png',
+                    height: size.height * 0.2,
+                    boxFit: BoxFit.fill,
+                    errorWidget: const Icon(
+                      IconlyBold.danger,
+                      color: Colors.red,
+                      size: 22,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Titiel',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.001,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
